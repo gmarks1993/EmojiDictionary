44ab4a1ba354cea911d02e2b8c4c642bd2173f3b
Grant Marks <grant@Grants-MacBook-Air-2.local>
2017-09-06 12:21:54 -0500

    First Commit

---
+++ EmojiDictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmojiDictionary
+//
+//  Created by Grant Marks on 9/6/17.
+//  Copyright © 2017 Grant Marks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
